7c068fcf136d5a2a68b20d2ed168caeb4f2a6aff
Clovis <clovisgladstone@gmail.com>
2024-07-09 11:30:43 -0500

    rename lg tag types

--- sarvangi_complete_tei.py
+++ sarvangi_complete_tei.py
@@ -15,17 +15,31 @@ new_header_content = """
   </sourceDesc>
 </teiHeader>
 """
+
+lg_map = {
+    "[S]": "sākhī",
+    "[P]": "pada",
+    "[K]": "kavitta",
+    "[A]": "arilla",
+    "[Ś]": "śloka",
+    "[C]": "caupāī",
+    "[SAVAI]": "savaiye",
+}
+
 tei = """<?xml version="1.0" encoding="utf-8"?>
 <TEI xmlns="http://www.tei-c.org/ns/1.0">"""
 tei += new_header_content
-tei+="<text><body>"
+tei += "<text><body>"
 for i in range(1, 127):
     file_path = f"section{i}.xml"
     with open(file_path, "r", encoding="utf-8") as file:
         section_content = file.read()
     soup = BeautifulSoup(section_content, "xml")
+    for lg in soup.find_all("lg"):
+        if "type" in lg.attrs:
+            lg["type"] = lg_map.get(lg["type"], lg["type"]).title()
     tei += soup.div.prettify()
-tei+="</text></body>"    
+tei += "</text></body>"
 tei += "</TEI>"
 
 soup = BeautifulSoup(tei, "xml")
